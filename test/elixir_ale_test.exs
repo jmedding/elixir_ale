e02b98fd86c5c984dfe06a81f559eb3e5c9705b8
Jon Medding <jonmedding@gmail.com>
2016-09-29 06:20:20 +0200

    pin 7 is gpio 4git push

--- test/elixir_ale_test.exs
+++ test/elixir_ale_test.exs
@@ -1,9 +1,10 @@
 defmodule ElixirAleTest do
   use ExUnit.Case
 
-  test "try to sense in pin7" do
-{:ok, pid} = DHT11.start_link 7
-DHT11.sense pid
+  test "try to sense in GPIO 4" do
+    #GPIO 4 is pin 7 on the digrams
+    {:ok, pid} = DHT11.start_link 4
+    DHT11.sense pid
 
     assert 1 + 1 == 2
   end
