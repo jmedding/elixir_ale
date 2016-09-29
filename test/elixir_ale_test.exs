6c89a391fc9350feb04707fe91c15126b3e93a45
Jon Medding <jonmedding@gmail.com>
2016-09-29 06:09:19 +0200

    added test

--- test/elixir_ale_test.exs
+++ test/elixir_ale_test.exs
@@ -1,7 +1,10 @@
 defmodule ElixirAleTest do
   use ExUnit.Case
 
-  test "the truth" do
+  test "try to sense in pin7" do
+{:ok, pid} = DHT11.start_link 7
+DHT11.sense pid
+
     assert 1 + 1 == 2
   end
 end
